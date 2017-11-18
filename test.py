15d07d884599055b4241d0ea736bea200bcbdfed
PJovy <pengjovy@gmail.com>
2017-11-18 19:10:56 +0800

    add test.py now it's convenient to test this app

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/18 18:53
+# @Author  : Jovy
+# @Site    : 
+# @File    : main.py
+# @Software: PyCharm Community Edition
+import xici_api
+
+
+def main():
+    xici_api.main()
+
+
+if __name__ == '__main__':
+    main()
